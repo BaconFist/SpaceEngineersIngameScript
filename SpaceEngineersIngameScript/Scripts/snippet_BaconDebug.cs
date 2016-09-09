50368cce45d55a81b02ec3cb5feba00ebe7ccbe4
Thomas Klose <thomas@bratler.net>
2016-09-10 00:04:51 +0200

    new Version of Debugger
    new features:
    - verbosity (eg: INFO, WARN)
    - .remainingInstructions
    - GridProgram public readable for easy acces
    - autoscrolling can be turned off
    API CHANGES:
    renamed: putSender => newScope
    renamed: pullSender => leaveScope

--- SpaceEngineersIngameScript/Scripts/snippet_BaconDebug.cs
+++ SpaceEngineersIngameScript/Scripts/snippet_BaconDebug.cs
@@ -24,44 +24,67 @@ namespace snippet_BaconDebug
         Copyright 2016 Thomas Klose <thomas@bratler.net>
         License: https://github.com/BaconFist/SpaceEngineersIngameScript/blob/master/LICENSE
 
-        Description
-        ===========
-            argument:  lcdtag originname message~----
+        
         */
 
         public class BaconDebug
         {
-            private List<IMyTextPanel> Panels = new List<IMyTextPanel>();
-            private MyGridProgram PB;
-            private List<string> callsStack = new List<string>();
-            private bool enabled = false;
+            public const int INFO = 3;
+            public const int WARN = 2;
+            public const int ERROR = 1;
+            public const int DEBUG = 4;
 
-            public BaconDebug(string tag, IMyGridTerminalSystem GTS, MyGridProgram PB, bool enabled)
+            private List<IMyTextPanel> Panels = new List<IMyTextPanel>();
+            private MyGridProgram GridProgram;
+            private List<string> callsStack = new List<string>();
+            private int verbosity = 0;
+            private bool _autoscroll = true;
+
+            public int remainingInstructions {
+                get { return GridProgram.Runtime.MaxInstructionCount - GridProgram.Runtime.CurrentInstructionCount; }
+            }
+
+            public bool autoscroll
             {
+                get { return _autoscroll; }
+                set { _autoscroll = value; }
+            }
+
+            public void clearPanels()
+            {
+                for(int i = 0; i < Panels.Count; i++)
+                {
+                    Panels[i].WritePublicText("");
+                }
+            }
+
+            public BaconDebug(string tag, IMyGridTerminalSystem GTS, MyGridProgram PB, int verbosity)
+            {
+                this.verbosity = verbosity;
                 List<IMyTerminalBlock> slug = new List<IMyTerminalBlock>();
                 GTS.GetBlocksOfType<IMyTextPanel>(slug, ((IMyTerminalBlock x) => x.CustomName.Contains(tag) && x.CubeGrid.Equals(PB.Me.CubeGrid)));
                 Panels = slug.ConvertAll<IMyTextPanel>(x => x as IMyTextPanel);
-                this.PB = PB;
-                putSender("BaconDebug");
-                setEnabled(enabled);                
+                this.GridProgram = PB;
+                newScope("BaconDebug");
             }
 
-            private void setEnabled(bool e)
+            public int getVerbosity()
             {
-                this.enabled = e;
+                return verbosity;
             }
 
-            private void updatePanels(string tag, IMyGridTerminalSystem GTS)
+            public MyGridProgram getGridProgram()
             {
-                
+                return this.GridProgram;
             }
+            
 
-            public void putSender(string sender)
+            public void newScope(string sender)
             {
                 callsStack.Add(sender);
             }
 
-            public void pullSender()
+            public void leaveScope()
             {
                 if (callsStack.Count > 1)
                 {
@@ -73,25 +96,36 @@ namespace snippet_BaconDebug
                 return callsStack[callsStack.Count-1];
             }
 
-            public void add(string msg)
+            public void add(string msg, int verbosity)
             {
-                if (enabled)
+                if (verbosity <= this.verbosity)
                 {
+                    string debugMsg = buildLine(msg);
+                    if (verbosity == ERROR)
+                    {
+                        GridProgram.Echo(debugMsg);
+                    }
                     for (int i = 0; i < Panels.Count; i++)
                     {
-                        List<string> lines = new List<string>();
-                        lines.AddRange(Panels[i].GetPublicText().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
-                        StringBuilder sb = new StringBuilder();
-                        lines.Add(buildLine(msg));
-                        if (!Panels[i].GetPrivateTitle().ToLower().Equals("nolinelimit"))
+                        if (autoscroll)
                         {
-                            int maxLines = getMaxLines(Panels[i]);
-                            if (lines.Count > maxLines)
+                            List<string> lines = new List<string>();
+                            lines.AddRange(Panels[i].GetPublicText().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
+                            StringBuilder sb = new StringBuilder();
+                            lines.Add(debugMsg);
+                            if (!Panels[i].GetPrivateTitle().ToLower().Equals("nolinelimit"))
                             {
-                                lines.RemoveRange(0, lines.Count - maxLines);
+                                int maxLines = getMaxLines(Panels[i]);
+                                if (lines.Count > maxLines)
+                                {
+                                    lines.RemoveRange(0, lines.Count - maxLines);
+                                }
                             }
+                            Panels[i].WritePublicText(string.Join("\n", lines));
+                        } else
+                        {
+                            Panels[i].WritePublicText(debugMsg + '\n', true);
                         }
-                        Panels[i].WritePublicText(string.Join("\n", lines));
                     }
                 }
             }
@@ -110,10 +144,11 @@ namespace snippet_BaconDebug
             private string buildLine(string msg)
             {
                 StringBuilder slug = new StringBuilder();
-                slug.Append("[" + DateTime.Now.ToShortTimeString() + "]");
+
+
+                slug.Append("[" + DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToShortTimeString() + "." + DateTime.Now.Millisecond.ToString().TrimStart('0') + "]");
                 slug.Append("[" + getSender() + "]");
-                slug.Append("[IC " + PB.Runtime.CurrentInstructionCount + "/" + PB.Runtime.MaxInstructionCount + "]");
-                slug.Append("[MCC " + PB.Runtime.CurrentMethodCallCount + "/" + PB.Runtime.MaxMethodCallCount + "]");
+                slug.Append("[IC " + GridProgram.Runtime.CurrentInstructionCount + "/" + GridProgram.Runtime.MaxInstructionCount + "]");
                 slug.Append(" " + msg);
 
                 return slug.ToString();
@@ -123,7 +158,7 @@ namespace snippet_BaconDebug
 
         public class min
         {
-            public class BaconDebug { List<IMyTextPanel> g = new List<IMyTextPanel>(); MyGridProgram h; List<string> i = new List<string>(); bool j = false; public BaconDebug(string a, IMyGridTerminalSystem b, MyGridProgram c, bool d) { var e = new List<IMyTerminalBlock>(); b.GetBlocksOfType<IMyTextPanel>(e, ((IMyTerminalBlock f) => f.CustomName.Contains(a) && f.CubeGrid.Equals(c.Me.CubeGrid))); g = e.ConvertAll<IMyTextPanel>(f => f as IMyTextPanel); this.h = c; putSender("BaconDebug"); k(d); } void k(bool a) { this.j = a; } void l(string a, IMyGridTerminalSystem b) { } public void putSender(string a) { i.Add(a); } public void pullSender() { if (i.Count > 1) i.RemoveAt(i.Count - 1); } public string getSender() { return i[i.Count - 1]; } public void add(string a) { if (j) for (int b = 0; b < g.Count; b++) { List<string> c = new List<string>(); c.AddRange(g[b].GetPublicText().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)); StringBuilder d = new StringBuilder(); c.Add(n(a)); if (!g[b].GetPrivateTitle().ToLower().Equals("nolinelimit")) { int e = m(g[b]); if (c.Count > e) { c.RemoveRange(0, c.Count - e); } } g[b].WritePublicText(string.Join("\n", c)); } } int m(IMyTextPanel a) { float b = a.GetValueFloat("FontSize"); if (b == 0.0f) b = 0.01f; return Convert.ToInt32(Math.Ceiling(17.0f / b)); } string n(string a) { var b = new StringBuilder(); b.Append("[" + DateTime.Now.ToShortTimeString() + "]"); b.Append("[" + getSender() + "]"); b.Append("[IC " + h.Runtime.CurrentInstructionCount + "/" + h.Runtime.MaxInstructionCount + "]"); b.Append("[MCC " + h.Runtime.CurrentMethodCallCount + "/" + h.Runtime.MaxMethodCallCount + "]"); b.Append(" " + a); return b.ToString(); } }
+            public class BaconDebug { public const int INFO = 3; public const int WARN = 2; public const int ERROR = 1; public const int DEBUG = 4; List<IMyTextPanel> h = new List<IMyTextPanel>(); MyGridProgram i; List<string> j = new List<string>(); int k = 0; bool l = true; public int remainingInstructions { get { return i.Runtime.MaxInstructionCount - i.Runtime.CurrentInstructionCount; } } public bool autoscroll { get { return l; } set { l = value; } } public void clearPanels() { for (int a = 0; a < h.Count; a++) h[a].WritePublicText(""); } public BaconDebug(string a, IMyGridTerminalSystem b, MyGridProgram c, int d) { this.k = d; var e = new List<IMyTerminalBlock>(); b.GetBlocksOfType<IMyTextPanel>(e, ((IMyTerminalBlock f) => f.CustomName.Contains(a) && f.CubeGrid.Equals(c.Me.CubeGrid))); h = e.ConvertAll<IMyTextPanel>(f => f as IMyTextPanel); this.i = c; newScope("BaconDebug"); } public int getVerbosity() { return k; } public MyGridProgram getGridProgram() { return this.i; } public void newScope(string a) { j.Add(a); } public void leaveScope() { if (j.Count > 1) j.RemoveAt(j.Count - 1); } public string getSender() { return j[j.Count - 1]; } public void add(string a, int b) { if (b <= this.k) { var c = n(a); if (b == ERROR) i.Echo(c); for (int d = 0; d < h.Count; d++) if (autoscroll) { List<string> e = new List<string>(); e.AddRange(h[d].GetPublicText().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries)); StringBuilder f = new StringBuilder(); e.Add(c); if (!h[d].GetPrivateTitle().ToLower().Equals("nolinelimit")) { int g = m(h[d]); if (e.Count > g) { e.RemoveRange(0, e.Count - g); } } h[d].WritePublicText(string.Join("\n", e)); } else { h[d].WritePublicText(c + '\n', true); } } } int m(IMyTextPanel a) { float b = a.GetValueFloat("FontSize"); if (b == 0.0f) b = 0.01f; return Convert.ToInt32(Math.Ceiling(17.0f / b)); } string n(string a) { var b = new StringBuilder(); b.Append("[" + DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToShortTimeString() + "." + DateTime.Now.Millisecond.ToString().TrimStart('0') + "]"); b.Append("[" + getSender() + "]"); b.Append("[IC " + i.Runtime.CurrentInstructionCount + "/" + i.Runtime.MaxInstructionCount + "]"); b.Append(" " + a); return b.ToString(); } }
         }
     }
 }
